c9f71d2561ad2c76ff81d4464ec5fedcc88204dc
davidjvg <36414398+davidjvg@users.noreply.github.com>
2020-04-22 15:29:29 +0200

    .

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ provider "azurerm" {
 
  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version  = "=1.42.0"
-  client_id = client_id
-  subscription_id = subscription_id
-  tenant_id = tenant_id
-  client_secret = client_secret
+#  client_id = client_id
+#  subscription_id = subscription_id
+#  tenant_id = tenant_id
+#  client_secret = client_secret
 
 }
