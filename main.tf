387bf42c80e02c32698b68803d31f00446614b6c
davidjvg <36414398+davidjvg@users.noreply.github.com>
2020-04-22 18:37:23 +0200

    .

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
   version  = "=1.42.0"
   #client_id = var.client_id
   #subscription_id = var.subscription_id
-  #tenant_id = var.tenant_id
+  tenant_id = var.tenant_id
   #client_secret = var.client_secret
 
 
