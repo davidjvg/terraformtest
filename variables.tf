c2b6d5d49fbe885b864068e0ed3c12fe03aef5bc
davidjvg <36414398+davidjvg@users.noreply.github.com>
2020-04-22 15:12:14 +0200

    .

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 # CREDENTIALS
-#variable "client_id" {}
+variable "client_id" {}
 
-#variable "client_secret" {}
+variable "client_secret" {}
 
-#variable "tenant_id" {}
+variable "tenant_id" {}
 
-#variable "subscription_id" {}
+variable "subscription_id" {}
 
 
 # NAME OF INSTANCE
